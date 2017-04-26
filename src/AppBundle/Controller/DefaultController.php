f1d587394e26a32ecf13df2bc12d76234d4bb816
mladen <mladen.ocr@gmail.com>
2017-04-27 01:27:39 +0200

    fixed bug on stylesheet path for app/Resources/view/base.html.twig

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,6 +14,6 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('AppBundle::index.html.twig');
+        return $this->render('AppBundle:Default:index.html.twig');
     }
 }
